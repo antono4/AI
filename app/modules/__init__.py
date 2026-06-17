148c3e5c51db4a7d56250bbc65dc7fe0c4381867
antono <73228642+antono4@users.noreply.github.com>
2026-06-17 23:33:23 +0700

    Remove app/modules/__init__.py

--- app/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-# App modules
-from .video_generator import VideoProject, ContentGenerator, StockVideoFetcher, create_video_from_topic
-from .voiceover import VoiceoverGenerator, BackgroundMusic, create_complete_audio
-from .video_editor import VideoProcessor, CaptionGenerator, auto_process_video
-from .youtube_uploader import YouTubeUploader, YouTubeAuth, ThumbnailGenerator, quick_upload
-from .scheduler import AutomationScheduler, get_scheduler, start_automation, stop_automation
